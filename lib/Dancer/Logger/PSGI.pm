2d615db13d2dbe9f134d8cc59f0b770282bf35b6
franck cuny <franck@lumberjaph.net>
2010-04-17 13:23:08 +0200

    log to psgix.logger

--- lib/Dancer/Logger/PSGI.pm
+++ lib/Dancer/Logger/PSGI.pm
@@ -4,26 +4,54 @@ use strict;
 use warnings;
 our $VERSION = '0.01';
 
+use Dancer::SharedData;
+use base 'Dancer::Logger::Abstract';
+
+sub init {}
+
+sub _log {
+    my ( $self, $level, $message ) = @_;
+    my $request = Dancer::SharedData->request;
+    if ( $request->{env}->{'psgix.logger'} ) {
+        $request->{env}->{'psgix.logger'}
+            ->( { level => $level, message => $message } );
+    }
+}
+
 1;
 __END__
 
 =head1 NAME
 
-Dancer::Logger::PSGI -
+Dancer::Logger::PSGI - PSGI logger handler for Dancer
 
 =head1 SYNOPSIS
 
-  use Dancer::Logger::PSGI;
+In your Dancer's configuration file:
+
+    logger: PSGI
+
+In your application
+
+    warning "this is a warning"
+
+In your app.psgi
+
+    $app = builder { enable "ConsoleLogger"; $app; }
+
+With L<Plack::Middleware::ConsoleLogger>, all your log will be send to the javascript console of your browser.
 
 =head1 DESCRIPTION
 
-Dancer::Logger::PSGI is
+This class is an interface between your Dancer's application and B<psgix.logger>. Message will be logged in whatever logger you decided to use in your L<Plack> handler. If no logger is defined, nothing will be logged.
 
 =head1 AUTHOR
 
 franck cuny E<lt>franck@lumberjaph.netE<gt>
 
 =head1 SEE ALSO
+
+L<Plack::Middleware::ConsoleLogger>, L<Plack::Middleware::NullLogger>, L<Plack::Middleware::Log4perl>.
 
 =head1 LICENSE
 
