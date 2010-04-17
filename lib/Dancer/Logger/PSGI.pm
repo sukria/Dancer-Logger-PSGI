6c4c80f08532fc4a7b897b62f7035d35788f3985
franck cuny <franck@lumberjaph.net>
2010-04-17 12:38:20 +0200

    initial commit

---
+++ lib/Dancer/Logger/PSGI.pm
@@ -0,0 +1,33 @@
+package Dancer::Logger::PSGI;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Dancer::Logger::PSGI -
+
+=head1 SYNOPSIS
+
+  use Dancer::Logger::PSGI;
+
+=head1 DESCRIPTION
+
+Dancer::Logger::PSGI is
+
+=head1 AUTHOR
+
+franck cuny E<lt>franck@lumberjaph.netE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
