7ccf1af1177a9285c03f0a31fb3d96e7c5a2346c
Alexis Sukrieh <sukria@sukria.net>
2010-06-20 13:25:06 +0200

    use format_message to provide a full featured log message

--- lib/Dancer/Logger/PSGI.pm
+++ lib/Dancer/Logger/PSGI.pm
@@ -15,9 +15,12 @@ sub init {}
 sub _log {
     my ( $self, $level, $message ) = @_;
     my $request = Dancer::SharedData->request;
-    if ( $request->{env}->{'psgix.logger'} ) {
-        $request->{env}->{'psgix.logger'}
-            ->( { level => $level, message => $message } );
+    if ($request->{env}->{'psgix.logger'}) {
+        $request->{env}->{'psgix.logger'}->(
+            {   level   => $level,
+                message => $self->format_message($level => $message)
+            }
+        );
     }
 }
 
